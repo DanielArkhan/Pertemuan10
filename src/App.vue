ff431aeb691d1f2dd171c7bcb2ec2851510f4bf0
Daniel Arkhan <danielarkhan@student.uir.ac.id>
2025-05-25 22:13:57 +0700

    Setup file main.js dan layout utama di App.vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+// File: App.vue
+<template>
+  <div style="display: flex">
+    <Sidebar />
+    <main class="main-content">
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script>
+import Sidebar from './components/Sidebar.vue';
+export default {
+  components: { Sidebar }
+}
+</script>
